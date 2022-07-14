5f96e00dbae9bacbbc9c8cef8eaf1a39b3f0eb13
Mac Mini <macmini@Macs-Mac-mini-7.local>
2022-07-14 16:47:00 +0600

    provider

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:api_provider/category_provider/category_homepage.dart';
+import 'package:api_provider/category_provider/category_provider.dart';
+import 'package:api_provider/category_withoutProvider/category_HomePage1.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+    ChangeNotifierProvider<CategoryProvider>(create: (BuildContext)=>CategoryProvider()
+    ), ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
+
